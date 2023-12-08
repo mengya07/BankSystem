a10d2845cf74be8cefc29111e713807e18d42ea5
flownia <110363328+flownia@users.noreply.github.com>
2023-12-08 17:05:56 +0800

    shiyan3

--- user/pages/myself/myself.vue
+++ user/pages/myself/myself.vue
@@ -1,22 +1,42 @@
 <template>
 	<view>
-		<view class="avatar-box">
-			<view class="avatar">
-				<uv-avatar src="../../static/logo.png" @click="clickAvatar"></uv-avatar>
-			</view>
-			<view class="text1">
-				{{timePeriod}}好,name
-			</view>
-		</view>
+		<uv-row class="avatar-box">
+			<uv-col span="3">
+				<view class="avatar">
+					<uv-avatar text="狗" random-bg-color size=50></uv-avatar>
+				</view>
+			</uv-col>
+			<uv-col span="9">
+				<view class="avatar-text">
+					{{timePeriod}}好，name
+				</view>
+			</uv-col>
+		</uv-row>
 		
-		<uv-grid :border="false">
-			<uv-grid-item v-for="(item,index) in girdList" :key="index">
-				<text class="grid-text">{{item.title}}</text>
+		
+		<view class="gird-box">
+		<uv-grid :border="false" :col="4">
+			<uv-grid-item>
+				<uv-icon :customStyle="{paddingTop:20+'rpx'}" name="/static/icon/icon_account.svg" :size="32"></uv-icon>
+				<text class="grid-text" >账户</text>
+			</uv-grid-item>
+			<uv-grid-item>
+				<uv-icon :customStyle="{paddingTop:20+'rpx'}" name="/static/icon/icon_record.svg" :size="32"></uv-icon>
+				<text class="grid-text" >交易明细</text>
+			</uv-grid-item>
+			<uv-grid-item @click="clickTransferRecord">
+				<uv-icon :customStyle="{paddingTop:20+'rpx'}" name="/static/icon/icon_transfer.svg" :size="32"></uv-icon>
+				<text class="grid-text" >转账</text>
+			</uv-grid-item>
+			<uv-grid-item>
+				<uv-icon :customStyle="{paddingTop:20+'rpx'}" name="grid" :size="32"></uv-icon>
+				<text class="grid-text" >全部</text>
 			</uv-grid-item>
 		</uv-grid>
+		</view>
 		
 		<uv-list class="list" border>
-		    <uv-list-item class="list-item" title="我的账户" border></uv-list-item>
+		    <uv-list-item class="list-item" title="我的账户" border ></uv-list-item>
 		    <uv-list-item class="list-item" title="安全与设置" border></uv-list-item>
 			<uv-list-item class="list-item" title="收支记录" border></uv-list-item>
 			<uv-list-item class="list-item" title="关于我们" border></uv-list-item>
@@ -30,20 +50,13 @@
 		data() {
 			return {
 				timePeriod: "下午",
-				girdList:[{
-					title: "账户"
-				},{
-					title: "交易明细"
-				},{
-					title: "转账"
-				},{
-					title: "全部"
-				}]
 			};
 		},
 		methods:{
-			clickAvatar(){
-				//头像设置 
+			clickTransferRecord(){
+				uni.navigateTo({
+					url:"/pages/transferRecord/transferRecord"
+				})
 			}
 		}
 	}
@@ -53,16 +66,30 @@
 .avatar-box{
 	height: 200rpx;
 	width: 100%;
-	display: flex;
-    align-items: center;
-	justify-content: center;
 	background-color: #3c9cff;
-	.avatar-box.avatar{
-		
+	backdrop-filter: blur(16px) saturate(180%);
+	-webkit-backdrop-filter: blur(16px) saturate(180%);
+	border-radius: 8px;
+	border: 1px solid rgba(209, 213, 219, 0.3);
+	.avatar{
+		margin-left: 50rpx;
+	}
+	.avatar-text{
+		margin-left: -20rpx;
+	}
+}
+.gird-box{
+	height: 140rpx;
+	margin: 10rpx;
+	border-radius: 10rpx;
+	background-color: #FFFFFF;
+	.grid-text{
+		font-size: 14px;
+		color: "black";
 	}
 }
 .list{
-	margin-top: 60rpx;
+	margin-top: 50rpx;
 	margin-left: 10rpx;
 	margin-right: 10rpx;
 	padding-left: 10rpx;
---
+++ user/pages/transferRecord/transferRecord.vue
@@ -0,0 +1,91 @@
+<template>
+	<view>
+		<view class="screen-box">
+			<view class="column1">近{{textScreenDate}}查询结果</view>
+			<view class="column2" @click="openScreen">
+				<text>筛选</text>
+				<uv-icon name="/static/icon/icon_screen.svg" size=22></uv-icon>
+			</view>
+		</view>
+		
+		<view class="record-box">
+			<view v-for="(item,index) in recordItem" :key="index" class="record-item">
+				<view class="column1">
+					余额{{item.balance}}
+				</view>
+				<view class="column2">
+					<view>{{item.otherName}}</view>
+					<view>{{item.amount}}</view>
+				</view>
+			</view>
+		</view>
+		
+		<uni-popup ref="popup" type="right" background-color="#ffffff">
+			<view>出淤泥而不染，濯清涟而不妖</view>
+		</uni-popup>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				textScreenDate:"3个月",
+				show: false,
+				recordItem:[{
+					date:"2023",
+					amount:"12.1",
+					balance:"1002.65",
+					otherName:"马化腾",
+				},{
+					date:"2023",
+					amount:"12.1",
+					balance:"1002.65",
+					otherName:"马化腾",
+				}]
+			};
+		},
+		methods:{
+			openScreen(){
+				this.show = true
+				this.$refs.popup.open()
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.screen-box{
+		margin-top: 20rpx;
+		display: flex;
+		justify-content: space-between;
+		.column1{
+			margin-left: 20rpx;
+		}
+		.column2{
+			margin-right: 20rpx;
+			display: flex;
+		}
+	}
+	.record-box{
+		margin-top: 20rpx;
+		.record-item{
+			height: 120rpx;
+			border-radius: 10rpx;
+			margin-left: 10rpx;
+			margin-right: 10rpx;
+			display: flex;
+			justify-content: space-between;
+			border-style: solid;
+			border-color: gray;
+			background-color: #FFFFFF;
+			.column1{
+				margin-left: 20rpx;
+			}
+			.column2{
+				margin-right: 20rpx;
+			}
+		}
+	}
+</style>
