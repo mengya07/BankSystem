ab4388e301197c24d5868c1fbf2bf38e04f24708
yuki7q <1017984273@qq.com>
2023-12-21 18:25:44 +0800

    111

--- user/pages/transferConfirm/transferConfirm.vue
+++ user/pages/transferConfirm/transferConfirm.vue
@@ -190,21 +190,25 @@ export default {
 										});
 									
 									 }
-									 else if(res.data.code=="300")
+									 else if(res.data.code=="110")
 									 {
-										 uni.showToast({
-										 	title:"请输入正确的转账信息",
-										 	icon:"none"
-										 })
-									 }
-									 else {
-										  
-										  
+										
 										  uni.showToast({
 										  	title:"验证码错误",
 										  	icon:"none"
 										  })
 										  
+										 
+									 }
+									 else {
+										 
+										 
+										   uni.showToast({
+										 	title:"请输入正确的转账信息",
+										 	icon:"none"
+										 })
+										  
+										 
 										  
 										}
 								  },  
