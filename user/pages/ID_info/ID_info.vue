9e4e4ebf751032890cf6d01ca87416de01388035
kallwaa <2028260566@qq.com>
2023-12-21 17:47:49 +0800

    kallwaa

---
+++ user/pages/ID_info/ID_info.vue
@@ -0,0 +1,213 @@
+<template>
+	<view class="title">
+	
+		<view class="info">
+			填写身份信息
+		</view>
+		<view class="tixin">
+			您已成功注册手机银行，填写身份信息绑定银行卡，畅享更多服务
+		</view>
+		
+		<input class="xing"  v-model="surname" maxlength="10" placeholder="请输入姓" />
+		<input class="ming" v-model="name" maxlength="10" placeholder="请输入名" />
+		<view>
+			<input class="idtype" @click="clickbuttom" v-model="ID_type" placeholder="请选择证件类型"></input>
+			<u-picker :show="showpick" :columns="columns" @cancel="cancel" @confirm="confirm" @change="change"></u-picker>
+		</view>
+		
+		<input class="sfz" @click="showkey" v-model="ID" maxlength="19" placeholder="请输入证件号码" />
+		
+		
+		<view class="uni-list">
+			<checkbox-group @change="handleChange">
+			<checkbox class="checkbox-3" >
+				<text class="tonyi">
+					本人(客户)已仔细阅读并理解《中国银行股份有限公司手机银行服务协议》《中国银行股份有限公司个人电子限行风险提示》完全同意和接受协议书全部欧和内容，愿融腊行和承担该协议书中约定的权利和义务。
+				</text>
+			</checkbox>
+			</checkbox-group>
+		</view>
+		<button class="xbu" v-on:click="clickNUM">下一步</button>
+		<u-keyboard 
+		ref="uKeyboard"
+		 mode="card"  
+		 @change="valchange" 
+		 @backspace="backspace" 
+		 :show="show" 
+		 @cancel="Clickclose"
+		@confirm="clickconfirm" 
+		dotDisabled
+		:overlay="false"
+		></u-keyboard>
+		</view>
+</template>
+<script>
+import test from '../../../uni_modules/uview-ui/libs/function/test'
+	export default{	
+		data() {
+				return {
+				show:false,
+				showpick:false,
+				ifchecked:false,
+				tt:false,
+				surname:'',
+				name:'',
+				ID:'',
+				ID_type:'请选择证件类型',
+				test:'',
+				columns:[
+					['居民身份证','护照','港澳通行证']
+				],
+			}
+		},
+		methods:{
+			clickNUM:function(event){
+				let that=this
+				if(this.ifchecked){
+					uni.getStorage({
+							key:'token',
+							success(res) {
+								let _token=res.data
+								uni.request({
+									url:'https://120.55.37.93/register/identityVerification',
+									method:'POST',
+									header:{
+										"token" :_token
+									},
+									data:{
+										"surname":that.surname,
+										"name":that.name,
+										"identityCard":that.ID
+									},
+									success(data) {
+										console.log(data)
+									}
+								})
+							}
+					})
+						
+				}else{
+					uni.showModal({
+						title:"请同意相关协议"
+					})
+				}
+				if (this.surname.trim() === '') {
+				  uni.showModal({
+				    title: '姓不能为空',
+				  
+				  })
+				  }
+				  if (this.name.trim() === '') {
+				    uni.showToast({
+				      title: '名不能为空',
+				    
+				    })
+					}
+					
+			if(this.ID_type=='请选择证件类型'){
+				uni.showToast({
+					title:"请选择证件类型"
+				})
+			}
+			var p = /^[1-9]\d{5}(18|19|20)\d{2}((0[1-9])|(1[0-2]))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/;
+			if((p.test(this.ID))){
+				 let reg = /^([\u4e00-\u9fa5]{2,20}|[a-zA-Z.\s]{2,20})$/;
+				 var arr = [this.surname, this.name]; 
+				 this.test = arr.join('');
+				 if(!(reg.test(this.test))){
+					 uni.showToast({
+					 	title:"姓名不正确"
+					 })
+				 }
+			}
+			else{
+				uni.showModal({
+					title:"身份证错误"
+				})
+			}
+			},
+			clickbuttom(){
+				this.showpick=true
+			},
+			handleChange(e) {
+						if(this.ifchecked){
+						this.ifchecked=false;
+						}else{
+							this.ifchecked=true;
+						}
+					},
+					showkey(){
+						this.show=true
+					},
+					valchange(val){
+						this.ID+=val
+						uni.hideKeyboard();
+					},
+					backspace(){
+						if(this.ID.length) this.ID = this.ID.substr(0, this.ID.length - 1);
+						console.log(this.ID);
+					},
+					Clickclose(){
+					this.show=false
+					},
+					clickconfirm(){
+						this.show=false
+					},
+					cancel(){
+						this.showpick=false
+					},
+					confirm(index){
+						this.ID_type=String(index.value)
+						this.showpick=false
+					}
+					
+		}
+	}
+	
+</script>
+
+<style lang="scss">
+	.info{
+		font-size: 25px;
+		font-weight: bold;
+		margin: 40px 15px 40px 10px;
+	}
+	.tixin{
+		font-size: 15px;
+		margin: 10px 15px 40px 15px;
+		color: grey;
+	}
+	.xing{
+		margin:5px 15px 40px 15px ;
+		
+	}
+	.ming{
+		margin:5px 15px 40px 15px ;
+	}
+	.sfz{
+		margin:5px 15px 40px 15px ;
+	}
+	.uni-list{
+		width: 750rpx;
+		height: 200rpx;
+		margin: 50px 20px 20px 20px;
+		.checkbox-3{
+			
+			.tonyi{
+				width: 600rpx;
+				height: 200rpx;
+				font-size: 15px;
+			}
+		}
+	}
+	.xbu{
+		width: 650rpx;
+		color: white;
+		background-color: red;
+		border-radius: 25px;
+	}
+	.idtype{
+		margin:5px 15px 40px 15px ;
+		
+	}
+</style>
---
+++ user/pages/phoneNumber/phoneNumber.vue
@@ -0,0 +1,132 @@
+<template>
+	<view>
+		<view class="info">
+			手机号码验证
+		</view>
+		
+		<input class="phone" @input="shishi" v-model="phoneNUmber"  maxlength="11" placeholder="手机号(中国内地)" />
+		<view class="xiansi" v-if="xiansi" >请输入正确的手机号</view>
+		<button class="xb"  v-on:click="clickNUM">下一步</button>
+		<view class="uni-list">
+			<checkbox-group @change="handleChange">
+			<checkbox class="checkbox-3" >
+				<text class="tonyi">
+					我已阅读并接受《用户服务协议》《隐私政策》
+				</text>
+			</checkbox>
+			</checkbox-group>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				phoneNUmber:'',
+				xianshi:false,
+				xiansi:false,
+				ifchecked:false,
+				refresh:false
+			}
+			},
+			methods:{
+				clickNUM(){
+					let that = this
+					if(this.phoneNUmber.length===11&&this.ifchecked) {
+						
+						//uni.request({
+							//url:'https://120.55.37.93/sendsms/nologin?phoneNumber='+this.phoneNUmber,
+							//success:function(res){
+								//console.log(res)
+												uni.navigateTo({
+												url:"/pages/index/mima/test/test",
+												success: function(res){
+													res.eventChannel.emit('setData', that.phoneNUmber)
+												},	
+								})
+							//},
+						// 	fail:function(error){
+						// 		uni.showModal({
+						// 			title:'发送失败请检查网络设置'
+						// 		})
+						// 	}
+						// })
+				
+				}
+						else {
+							if(this.ifchecked){
+								uni.showModal({
+									title:'请输入正确的手机号'
+								})
+							}
+							else{
+							uni.showModal({
+								title:"请同意相关协议"
+							})
+							}
+					}
+				},
+				shishi(){
+					const phoneReg = /^[1][3-9]\d{9}$/;
+					 
+					// 获取输入框的值
+					const inputValue = this.phoneNUmber;
+					 
+					// 使用正则表达式进行匹配
+					const isPhoneValid = phoneReg.test(inputValue);
+					 
+					if(isPhoneValid){
+						this.xiansi=false;
+						console.log("1")
+					}
+					else{
+						this.xiansi=true;
+						console.log("2")
+					}
+				},
+				handleChange(e) {
+							if(this.ifchecked){
+							this.ifchecked=false;
+							}else{
+								this.ifchecked=true;
+							}
+						},
+			}
+		
+	}
+</script>
+
+<style lang="scss">
+	.info{
+		font-size: 25px;
+		font-weight: bold;
+		margin: 100px 15px 40px 10px;
+	}
+	.phone{
+		margin:5px 15px 10px 15px ;
+		border-bottom: 1px solid black;
+	}
+	
+	.xb{
+		width: 650rpx;
+		color: white;
+		background-color: red;
+		border-radius: 25px;
+		margin: 40px 50px 40px 10px;
+		left:4%;
+		position: fixed;
+		bottom:520px;
+	}
+	.xiansi{
+		color: red;
+		font-size: 15px;
+		margin: 10px 15px 40px 10px;
+	}
+	.uni-list{
+		bottom: 510px;
+		position:fixed;
+		margin:5px 15px 10px 15px ;
+	}
+
+</style>
