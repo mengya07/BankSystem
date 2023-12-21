3a3cc39be138d2fb5583cf9d2e986f64a57ef72e
yuki7q <1017984273@qq.com>
2023-12-21 18:18:46 +0800

    1111

--- user/pages/transfer/transfer.vue
+++ user/pages/transfer/transfer.vue
@@ -131,7 +131,7 @@
 							console.log(temp);		
 																																																		
 																																																		
-							uni.redirectTo({
+							uni.navigateTo({
 								url:'/pages/transferConfirm/transferConfirm'
 							})																																											
 																																																		
